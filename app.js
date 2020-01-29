9971bd71e2d342cf44400607ee2fe16db3d23059
VIncenzo Marcovecchio <vinceoldmark@gmail.com>
2020-01-29 16:40:32 +0100

    change improvement

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const form = document.querySelectorAll("form");
+const email = document.querySelectorAll(".email");
+const error = document.querySelectorAll(".email-address-error");
 function validateInput() {
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  const email = document.querySelectorAll(".email");
-  const error = document.getElementsByClassName("email-address-error");
-
-  if (email.all.value.lenght < 1 || !email.value.match(mailformat)) {
+  if (email.value.length < 1 || !email.value.match(mailformat)) {
     console.log("notValid email address");
     email.style.border = "1px solid hsl(354, 100%, 66%)";
     error.classList.add("active");
